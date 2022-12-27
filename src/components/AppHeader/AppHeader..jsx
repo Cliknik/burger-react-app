8aa54d1c3fdbef8beda9112e39cf03560c628633
cliknik <cliknik13@gmail.com>
2022-12-27 14:19:21 +0300

    feat: add appHeader

---
+++ src/components/AppHeader/AppHeader..jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import {Logo, BurgerIcon, ListIcon, ProfileIcon} from '@ya.praktikum/react-developer-burger-ui-components';
+
+import appHeaderStyles from './appheader.module.css';
+
+class AppHeader extends React.Component {
+    constructor(props) {
+        super(props);
+
+    }
+    render() {
+        return (
+            <header className={ `p-2 ${appHeaderStyles.header}`}>
+                <nav className={appHeaderStyles.navigation}>
+                    <div className={appHeaderStyles.container}>
+                        <div className={ `pl-5 ${appHeaderStyles.navItem}`}>
+                            <BurgerIcon type={"primary"} />
+                            <p className="text text_type_main-small pl-2">Конструктор</p>
+                        </div>
+                        <div className={ `pl-5 ${appHeaderStyles.navItem}`}>
+                            <ListIcon type={"secondary"} />
+                            <p className="text text_type_main-small text_color_inactive pl-2">Лента заказов</p>
+                        </div>
+                    </div>
+                    <Logo />
+                    <div className={ `pl-5 ${appHeaderStyles.navAccount}` } >
+                        <ProfileIcon type={"secondary"} />
+                        <p className="text text_type_main-small text_color_inactive pl-2">Личный кабинет</p>
+                    </div>
+                </nav>
+            </header>
+        )
+
+    }
+
+}
+
+export default AppHeader;
---
+++ src/components/BurgerConstructor/BurgerConstructor.jsx
@@ -0,0 +1,3 @@
+import React from "react";
+
+import constructorStyles from './burgerConstructor.module.css';
