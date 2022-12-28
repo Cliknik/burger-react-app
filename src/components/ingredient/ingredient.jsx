8dc685e6452f22856b27b4d46a68b4dca7044025
cliknik <cliknik13@gmail.com>
2022-12-28 13:39:13 +0300

    feat: add counter to Ingredient

--- src/components/ingredient/ingredient.jsx
+++ src/components/ingredient/ingredient.jsx
@@ -6,6 +6,7 @@ import styles from './ingredient.module.css'
 function Ingredient(props) {
     return (
         <div className={styles.ingredientContainer}>
+            <Counter count={1} extraClass="m-1" size="default" />
             <img src={props.image} alt="Картинка ингридиента"/>
             <p className={ `text text_type_digits-default ${styles.name}`}>{props.price} <CurrencyIcon type={"primary"} /></p>
             <p className={ `text text_type_main-small ${styles.name}`}>{props.name}</p>
