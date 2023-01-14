ecefae800a8f4707d5a3e64294f77074f0e9f6c5
cliknik <cliknik13@gmail.com>
2023-01-14 22:13:26 +0300

    fix: remove modalToggle

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -11,7 +11,7 @@ const url = 'https://norma.nomoreparties.space/api/ingredients'
 function App() {
     const [ingredients, setIngredients] = useState(null);
     const [modalOpened, setModalOpened] = useState(false);
-    const [modalContent, setModalContent] = useState(null);
+    const [modalType, setModalType] = useState(null);
     const [ingredientId, setIngredientId] = useState(null);
 
     const getProductData = () => {
@@ -35,26 +35,15 @@ function App() {
 
     function getModalType(evt){
         if (evt.currentTarget.className.includes('ingredient')) {
-            setModalContent('ingredient');
+            setModalType('ingredient');
         }
         else {
-            setModalContent('order');
+            setModalType('order');
         }
     }
 
     function getClickedIngredientId(evt) {
         setIngredientId(evt.currentTarget.id)
-    }
-
-    function toggleModalHandler(evt) {
-        setModalOpened(!modalOpened);
-        if (evt.currentTarget.className.includes('ingredient')) {
-            setModalContent('ingredient');
-            setIngredientId(evt.currentTarget.id)
-        }
-        else {
-            setModalContent('order')
-        }
     }
 
     function escButtonHandler(event){
@@ -79,7 +68,7 @@ function App() {
                 </>
             }
             </main>
-            <ModalLayout ingredientId={ingredientId} ingredients={ingredients} modalContent={modalContent} modalOpened={modalOpened} openModal={openModal} closeModal={closeModal} escButtonHandler={escButtonHandler}/>
+            <ModalLayout ingredientId={ingredientId} ingredients={ingredients} modalType={modalType} modalOpened={modalOpened} openModal={openModal} closeModal={closeModal} escButtonHandler={escButtonHandler}/>
         </div>
       </StrictMode>
   );
