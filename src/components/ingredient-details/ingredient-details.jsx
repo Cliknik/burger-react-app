f4290c912cb103ed2ae838f344910e1db59a25ed
cliknik <cliknik13@gmail.com>
2023-01-14 14:03:15 +0300

    feat: add IngredientDetail and OrderDetail

--- src/components/ingredient-details/ingredient-details.jsx
+++ src/components/ingredient-details/ingredient-details.jsx
@@ -3,17 +3,30 @@ import styles from './ingredient-details.module.css'
 function IngredientDetails({image, name, calories, proteins, fats, carbohydrates}){
     return(
         <>
-            <h1 className={`ml-10 mt-10 text text_type_main-large`}>Детали Ингридиента</h1>
+            <h2 className={`mt-10 text text_type_main-large ${styles.title}`}>Детали Ингридиента</h2>
             <div className={styles.contentContainer}>
-                <img alt={name} className={`mb-4`}/>
-                <h2 className={`mb-8`}>{name}</h2>
-                <ul>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
+                <img alt={name} className={`mb-4`} src='https://code.s3.yandex.net/react/code/bun-02-large.png'/>
+                <h3 className={`mb-8 text text_type_main-medium`}>Краторная булка N-200i</h3>
+                <ul className={styles.energyWorth}>
+                    <li className={styles.energyElement}>
+                        <p className={`text text_type_main-default text_color_inactive ${styles.energyName}`}>Каллории, ккал</p>
+                        <p className={`text text_type_digits-default text_color_inactive ${styles.energyName}`}>244,4</p>
+                    </li>
+                    <li className={styles.energyElement}>
+                        <p className={`text text_type_main-default text_color_inactive ${styles.energyName}`}>Белки, г</p>
+                        <p className={`text text_type_digits-default text_color_inactive ${styles.energyName}`}>12,2</p>
+                    </li>
+                    <li className={styles.energyElement}>
+                        <p className={`text text_type_main-default text_color_inactive ${styles.energyName}`}>Жиры, г</p>
+                        <p className={`text text_type_digits-default text_color_inactive ${styles.energyName}`}>17,2</p>
+                    </li>
+                    <li className={styles.energyElement}>
+                        <p className={`text text_type_main-default text_color_inactive ${styles.energyName}`}>Углеводы, г</p>
+                        <p className={`text text_type_digits-default text_color_inactive ${styles.energyName}`}>10,2</p>
+                    </li>
                 </ul>
             </div>
         </>
     )
 }
+export default IngredientDetails;
--- src/components/modal/modal.jsx
+++ src/components/modal/modal.jsx
@@ -1,7 +1,9 @@
 import React, {useState, useEffect} from "react";
 import {CloseIcon} from "@ya.praktikum/react-developer-burger-ui-components";
 
+
 import styles from './modal.module.css'
+import OrderDetails from "../order-details/order-details";
 
 export default function Modal({modalOpened}) {
     modalOpened = true;
@@ -10,7 +12,10 @@ export default function Modal({modalOpened}) {
         modalOpened &&
         <div className={styles.wrapper}>
             <div className={styles.modal}>
-                <CloseIcon type="primary" styles={{}}/>
+                <div className={styles.closeIcon}>
+                    <CloseIcon type="primary"/>
+                </div>
+                <OrderDetails/>
             </div>
         </div>
     )
