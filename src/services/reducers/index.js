ff49c03262937127f2f5ecca6058dd33de71fb54
cliknik <cliknik13@gmail.com>
2023-02-06 02:54:18 +0300

    feat: init rootReducer

---
+++ src/services/reducers/index.js
@@ -0,0 +1,12 @@
+import {combineReducers} from "redux";
+import {ingredientsDataReducer} from "./ingredientsData";
+import {orderReducer} from "./orderDetails";
+import {currentIngredientReducer} from './currentIngredient'
+import {constructorDataReducer} from './constructorData'
+
+export const rootReducer = combineReducers({
+    ingredients: ingredientsDataReducer,
+    constructor: constructorDataReducer,
+    ingredient: currentIngredientReducer,
+    order: orderReducer
+})
