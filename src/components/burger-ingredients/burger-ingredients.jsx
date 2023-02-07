d5cae22076e52dbdd9c3f5a3b79717e996e06289
cliknik <cliknik13@gmail.com>
2023-02-07 21:52:01 +0300

    feat: tabs are alive now!!! they obey to scroll!!!

--- src/components/burger-ingredients/burger-ingredients.jsx
+++ src/components/burger-ingredients/burger-ingredients.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useMemo, useContext} from "react";
+import React, {useState, useMemo, useRef} from "react";
 import PropTypes from "prop-types";
 import {Tab} from "@ya.praktikum/react-developer-burger-ui-components";
 
@@ -17,36 +17,78 @@ const BurgerIngredients = () => {
     const items = useSelector(store => store.ingredients.items)
     const modalOpened = useSelector(store => store.currentIngredient.modalOpened)
     const [current, setCurrent] = useState('one');
+    const [sectionScroll, setSectionScroll] = useState(0);
+
+    const bunSectionRef = useRef(null);
+    const saucesSectionRef = useRef(null);
+    const mainSectionRef = useRef(null);
+    const sectionRef = useRef(null);
 
     const buns = useMemo(() => items.filter(item => item['type'] === bunType),[items]);
     const sauces = useMemo(() => items.filter(item => item['type'] === sauceType),[items]);
     const fillings = useMemo(() => items.filter(item => item['type'] === mainType),[items]);
+
+    React.useEffect(() => {
+        sectionRef.current.addEventListener('scroll', handleScroll);
+        return () => sectionRef.current.removeEventListener('scroll', handleScroll);
+    }, [])
+
+    const handleScroll = () => {
+        setSectionScroll(sectionRef.current.scrollTop)
+    }
+
+    function setViewOnTab(target){
+        target.current.scrollIntoView({behavior: "smooth"})
+    }
+
+    function activateTab(){
+        if (sectionScroll >= 0 && sectionScroll <= 331 ) {
+            setCurrent('one');
+            return
+        }
+        else if (sectionScroll >= 332 && sectionScroll <= 867) {
+            setCurrent('two');
+            return
+        }
+        else {
+            setCurrent('three')
+        }
+    }
 
     return (
         <>
         <section className={Styles.section}>
             <h1 className="text text_type_main-large mt-10 mb-5">Соберите бургер</h1>
             <nav style={{ display: 'flex'}} className="mb-10">
-                <Tab value="one" active={current === 'one'} onClick={setCurrent} href="#buns" data-attribute-anchor="buns">
+                <Tab value="one" active={current === 'one'} onClick={() => {
+                    setCurrent('one');
+                    setViewOnTab(bunSectionRef)
+                }}>
                     Булки
                 </Tab>
-                <Tab value="two" active={current === 'two'} onClick={setCurrent} href="#sauces" data-attribute-anchor="sauces">
+                <Tab value="two" active={current === 'two'} onClick={() => {
+                    setCurrent('two');
+                    setViewOnTab(saucesSectionRef)
+                }}>
                     Соусы
                 </Tab>
-                <Tab value="three" active={current === 'three'} onClick={setCurrent} href="#main" data-attribute-anchor="main">
+                <Tab value="three" active={current === 'three'} onClick={() => {
+                    setCurrent('three');
+                    setViewOnTab(mainSectionRef)
+                }}>
                     Начинки
                 </Tab>
             </nav>
-            <div className={Styles.scrollSection}>
-                <h2 className="" id="buns">Булки</h2>
+            <div className={Styles.scrollSection} ref={sectionRef} onScroll={activateTab}>
+                <h2 className="" id="buns" ref={bunSectionRef}>Булки</h2>
                 <div className={Styles.ingredientsContainer}>
                     {buns.map(item => <Ingredient data={item} key={item['_id']} id={item['_id']} image={item['image']} price={item['price']} name={item['name']} />)}
                 </div>
-                <h2 id="sauces">Соусы</h2>
+                <h2 id="sauces" ref={saucesSectionRef}>Соусы</h2>
                 <div className={Styles.ingredientsContainer}>
                     {sauces.map(item => <Ingredient data={item} key={item['_id']} id={item['_id']} image={item['image']} price={item['price']} name={item['name']} />)}
                 </div>
-                <h2 id="main">Начинки</h2>
+                <h2 id="main" ref={mainSectionRef}>Начинки</h2>
                 <div className={Styles.ingredientsContainer}>
                     {fillings.map(item => <Ingredient data={item} key={item['_id']} id={item['_id']} image={item['image']} price={item['price']} name={item['name']} />)}
                 </div>
--- src/services/reducers/orderDetails.js
+++ src/services/reducers/orderDetails.js
@@ -1,1 +1,40 @@
-export function orderReducer(){};
+import {GET_ORDER_NUMBER_REQUEST, GET_ORDER_NUMBER_FAILED, GET_ORDER_NUMBER_SUCCESS, RESET_ORDER_NUMBER} from "../actions/orderDetails";
+
+const initialState = {
+    idRequest: false,
+    idSuccess: false,
+    orderID: '',
+    modalOpened: false
+}
+
+export const orderReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case GET_ORDER_NUMBER_REQUEST: {
+            return {
+                ...state,
+                idRequest: true
+            }
+        }
+        case GET_ORDER_NUMBER_SUCCESS: {
+            return {
+                ...state,
+                idRequest: false,
+                idSuccess: true,
+                orderID: action.payload,
+                modalOpened: true,
+            }
+        }
+        case GET_ORDER_NUMBER_FAILED: {
+            return {
+                ...state,
+                idRequest: false
+            }
+        }
+        case RESET_ORDER_NUMBER: {
+            return {
+                initialState
+            }
+        }
+        default: return state
+    }
+};
